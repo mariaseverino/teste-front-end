a04bf59bd5a645cf5d33bfe8a65908215a237530
Maria Severino <mariarsseverino@gmail.com>
2026-03-03 19:46:30 -0300

    feat: melhora titulo de seção

--- front/src/components/SectionTitle/index.tsx
+++ front/src/components/SectionTitle/index.tsx
@@ -2,14 +2,21 @@ import styles from './styles.module.scss';
 
 interface SectionTitleProps {
     title: string;
+    separator?: boolean;
 }
 
-export function SectionTitle({ title }: SectionTitleProps) {
+export function SectionTitle({ title, separator = true }: SectionTitleProps) {
     return (
         <div className={styles.upper}>
-            <div className={styles.separator}></div>
+            <div
+                className={styles.separator}
+                style={{ display: separator ? 'block' : 'none' }}
+            ></div>
             <h1>{title}</h1>
-            <div className={styles.separator}></div>
+            <div
+                className={styles.separator}
+                style={{ display: separator ? 'block' : 'none' }}
+            ></div>
         </div>
     );
 }
